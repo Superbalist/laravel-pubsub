5d8d82282c846dae2efcafdbfa02dbb46e1fba44
zeke <zekevictor@gmail.com>
2017-12-08 16:56:07 -0800

    kafka test run fixes

--- tests/PubSubConnectionFactoryTest.php
+++ tests/PubSubConnectionFactoryTest.php
@@ -126,14 +126,14 @@ class PubSubConnectionFactoryTest extends TestCase
             ->with($topicConf)
             ->once();
 
-        $container->shouldReceive('make')
+        $container->shouldReceive('makeWith')
             ->with('pubsub.kafka.conf')
             ->once()
             ->andReturn($conf);
 
         $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
 
-        $container->shouldReceive('make')
+        $container->shouldReceive('makeWith')
             ->withArgs([
                 'pubsub.kafka.consumer',
                 ['conf' => $conf],
